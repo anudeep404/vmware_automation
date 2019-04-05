fa88a1361b54873676b9d6f5c2533b1206296fc4
Anudeep Arkala <aarkala@vmware.com>
2019-04-05 21:07:29 +0530

    New Version_requries modification

--- 5_eam_analysis.py
+++ 5_eam_analysis.py
@@ -1,13 +1,8 @@
-#This progarm is built and tested on Python 3.7.1, please use the same version to avoid any run time issues.
-
 import os
-import re
-
 def chdir():
         new_dir=input("Please enter the new directory:")
         os.chdir(new_dir)
 chdir()
-
 file = input("Enter the filename:")
 Agency_list=[]
 Agent_list=[]
@@ -20,6 +15,13 @@ def eam_analysis():
         pattern = r'EsxAgentManager:agency['
         pattern1 = r'agent['
         pattern2 = r'host-'
+        pattern3 = r'baseOvfEnvironment'
+        pattern4 = r'controlNicKeyToIp'
+        pattern5 = r'config'
+        pattern6 = r'isUpgrading'
+        pattern7 = r'wasUpgrading'
+        pattern8 = r'rebootHostAfterVibUninstall'
+        pattern9 = r'goalState'
         print('\nThe list of Agencies under the Server GUID are as follows:\n')
         for line in open(file):
                 if pattern in line:
@@ -27,22 +29,87 @@ def eam_analysis():
                         print(Agency)
                         Agency_list.append(Agency)
         for Agency in Agency_list:
-                print('##################################')
-                print('\nThe Agents that are part of %s\n' %Agency)
+                print('###################################################################')
+                print('#The Agents that are part of %s' %Agency)
+                print('###################################################################')
                 for line in open(file):
                         if Agency in line:
                                 if pattern1 in line:
                                         Agent = line.split(',')[2].split(':')[3]
                                         print(Agent)
                                         Agent_list.append(Agent)
-        print('##################################')
-        print('##################################')
+        print('#########################################')
+        print('###Fetching the Host-ID for each Agent###')
+        print('#########################################')
         for Agent in Agent_list:
                 for line in open(file):
                         if Agent in line:
                                 if pattern2 in line:
                                         HostID = line.split(',')[2].split(':')[3]
-                                        print('\nThe host IDs of the Agent %s %s\n' % (Agent,HostID))
+                                        print('%s %s' %(Agent,HostID))
+        print('########################################')
+        print('##Fetching the Data Key foe each Agent##')
+        print('########################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern3 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
+        print('#################################################')
+        print('##Fetching the ControlNicKeyToIp for each Agent##')
+        print('#################################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern4 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
+        print('#################################################')
+        print('########Fetching the config for each Agent#######')
+        print('#################################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern5 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
+        print('#################################################')
+        print('#####Fetching the isUpgrading for each Agent#####')
+        print('#################################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern6 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
+        print('#################################################')
+        print('####Fetching the wasUpgrading for each Agent#####')
+        print('#################################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern7 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
+        print('#################################################')
+        print('##Fetching if reboot is required for each Agent##')
+        print('#################################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern8 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
+        print('#################################################')
+        print('######Fetching if goalstate for each Agent#######')
+        print('#################################################')
+        for Agent in Agent_list:
+                for line in open(file):
+                        if Agent in line:
+                                if pattern9 in line:
+                                        data_key = line.split(',')[2]
+                                        print('%s %s' %(Agent,data_key))
 eam_analysis()
 
 def main():
