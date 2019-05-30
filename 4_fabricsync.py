881651cc3f55e4d30fae620ce50d75e79acc5d1e
anudeep404 <anudeeparkala@gmail.com>
2019-05-30 08:53:07 +0530

    Changes to fabsync

--- 4_fabricsync.py
+++ 4_fabricsync.py
@@ -3,9 +3,9 @@
 import requests
 from xml.etree import ElementTree
 
-url1='https://nsxmgr01.core.hypervizor.com//api/2.0/nwfabric/resolveIssues/domain-c7'
-url2='https://nsxmgr01.core.hypervizor.com//api/2.0/nwfabric/resolveIssues/domain-c253'
-url3='https://nsxmgr02.core.hypervizor.com//api/2.0/nwfabric/resolveIssues/domain-c7'
+url1='https://nsxmgr01.corp.local.com//api/2.0/nwfabric/resolveIssues/domain-c77'
+url2='https://nsxmgr01.corp.local.com//api/2.0/nwfabric/resolveIssues/domain-c53'
+url3='https://nsxmgr02.corp.local.com//api/2.0/nwfabric/resolveIssues/domain-c76'
 
 nsxmanager_user='admin'
 nsxmanager_password='VMware1!'
