f4972fd1b3009ac4eadef204e2efe12aa511077a
Anudeep Arkala <aarkala@vmware.com>
2019-07-01 07:20:03 +0530

    bash

--- remote_command_bash.sh
+++ remote_command_bash.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sshpass -p 'VMware1!VMware1!' ssh -o StrictHostKeyChecking=no admin@192.168.110.190 "show status"
 sshpass -p 'VMware1!VMware1!' ssh -o StrictHostKeyChecking=no admin@192.168.110.191 "show status"
 sshpass -p 'VMware1!VMware1!' ssh -o StrictHostKeyChecking=no admin@192.168.110.192 "show status"
