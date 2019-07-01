0f5e446661f30c6dfddd939abdd30d84f1e17717
Anudeep Arkala <aarkala@vmware.com>
2019-07-01 07:18:35 +0530

    Execute command remotely on bash

---
+++ remote_command_bash.sh
@@ -0,0 +1,3 @@
+sshpass -p 'VMware1!VMware1!' ssh -o StrictHostKeyChecking=no admin@192.168.110.190 "show status"
+sshpass -p 'VMware1!VMware1!' ssh -o StrictHostKeyChecking=no admin@192.168.110.191 "show status"
+sshpass -p 'VMware1!VMware1!' ssh -o StrictHostKeyChecking=no admin@192.168.110.192 "show status"
